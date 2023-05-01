e44df2e4f11fef91fb06626ced3609a246b6099f
Jose Caio <48285880+JoseCaio44@users.noreply.github.com>
2023-05-01 17:55:52 -0300

    primeira API

---
+++ userdept/src/main/java/com/sistema/userdept/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.sistema.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistema.userdept.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
